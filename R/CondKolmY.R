0f55f6a65a1e9d28a28ad5c6b1522a64a9fd9982
Gitte Kremling <kremling@fh-aachen.de>
2024-02-14 17:06:47 +0100

    Added a new class or the new test statistic 'CondKolmY' and corresponding unit test

---
+++ R/CondKolmY.R
@@ -0,0 +1,36 @@
+##' @title Conditional Kolmogorov test statistic for the marginal distribution of Y
+##' @description This class implements a function of \code{TestStatistic} to calculate the test statistic (and x-y-values that can be used to plot it).
+##' @export
+CondKolmY <- R6::R6Class(
+  classname = "CondKolmY",
+  inherit = TestStatistic,
+  public = list(
+    #' @description Calculate the value of the test statistic for given data (x,y) and a model to test for
+    #'
+    #' @param x vector of covariates
+    #' @param y response variable
+    #' @param model model of type ParamRegrModel to test for
+    #'
+    #' @export
+    calc_stat = function(x, y, model) {
+      n <- length(y)
+      # compute ECDF of Y (non-parametric estimator for distribution of Y)
+      Fyn <- ecdf(y)
+
+      # determine jump points and value of the ECDF at these points
+      t.vals <- knots(Fyn)
+      Fyn.vals <- Fyn(t.vals)
+
+      # determine semi-parametric estimator for distribution of Y evaluated at the same (jump) points
+      Fypar.vals <- sapply(t.vals, function(t) { sum(model$F_yx(t, x))/n  })
+
+      # determine KS statistic by computing the difference at the jump points
+      proc <- sqrt(n) * (Fyn.vals-Fypar.vals)
+
+      # set private fields accordingly
+      private$value <- max(abs(proc))
+      private$plot.x <- t.vals
+      private$plot.y <- proc
+    }
+  )
+)
---
+++ tests/testthat/test-CondKolmY.R
@@ -0,0 +1,16 @@
+test_that("calc_stat works", {
+  set.seed(123)
+  g1 <- function(u) {return(exp(u))}
+  beta <- c(1,2,3)
+  sd <- 2
+  n <- 100
+  x <- rbind(runif(n), runif(n), rnorm(n))
+  y <- rnorm(n, mean=g1(beta %*% x), sd=sd)
+
+  model <- NormalGLM$new(g1)
+  model$fit(x,y, params_init=list(beta=c(1,2,3), sd=2))
+
+  test_stat <- CondKolmY$new()
+  test_stat$calc_stat(x, y, model)
+  expect_equal(test_stat$get_value(), 0.46643783)
+})
